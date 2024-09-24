8b1c286ece4dc7fa4cac8debafbe71f15553f76a
StaneKoz <98650254+StaneKoz@users.noreply.github.com>
2024-09-24 12:50:51 +0500

    Готовое задание с удалённой бд

--- db/init_db.js
+++ db/init_db.js
@@ -2,6 +2,12 @@ import pool from "./db.js"
 
 export default async function initializeDatabase() {
   try {
+    await pool.query(`CREATE TABLE IF NOT EXISTS players(
+      nickname VARCHAR(255),
+      email  VARCHAR(255),
+      registered INTEGER,
+      status VARCHAR(255));`);
+
     await pool.query('TRUNCATE players');
 
     const res = await pool.query('SELECT * FROM players');
